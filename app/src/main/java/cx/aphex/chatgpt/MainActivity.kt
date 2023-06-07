44817d18ba9fdcd880ed71a50033b27a15e613eb
afik.cohen <aphexsa@gmail.com>
2023-06-07 15:41:07 -0700

    Convert to jetpack compose.

--- app/src/main/java/cx/aphex/chatgpt/MainActivity.kt
+++ app/src/main/java/cx/aphex/chatgpt/MainActivity.kt
@@ -5,27 +5,49 @@ import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.text.SpannableStringBuilder
-import android.util.Log
-import android.view.KeyEvent
-import android.view.View
-import android.view.inputmethod.EditorInfo
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.aallam.openai.api.BetaOpenAI
-import cx.aphex.chatgpt.databinding.ActivityMainBinding
 import io.noties.markwon.Markwon
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
 @BetaOpenAI
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     private lateinit var pulsingAnimation: AnimatorSet
-    private lateinit var binding: ActivityMainBinding
+
+    //    private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
 
     private val markwon: Markwon by lazy { Markwon.create(this) }
@@ -38,83 +60,152 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    color = Color(
+                        ContextCompat.getColor(
+                            this,
+                            R.color.gpt4purple
+                        )
+                    ),
+                    modifier = Modifier.fillMaxHeight()
+                ) { // gpt4purple
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_chatgpt),
+                            contentDescription = "Logo",
+                            modifier = Modifier
+                                .size(64.dp)
+                                .padding(top = 24.dp)
+                        )
 
+                        var query by remember { mutableStateOf("") }
 
-        binding.searchQuery.setOnEditorActionListener { _, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH ||
-                (event?.keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN)
-            ) {
-                performSearch()
-                true
-            } else {
-                false
-            }
-        }
+                        OutlinedTextField(
+                            value = query,
+                            onValueChange = { newValue: String -> query = newValue },
+                            label = { Text("Search") },
+                            modifier = Modifier.fillMaxWidth(),
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(
+                                imeAction = ImeAction.Send,
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onSend = {
+                                    performSearch(query)
+                                }
+                            )
+                        )
 
-        blinkingCaretSpan = BlinkingCaretSpan(ContextCompat.getColor(this, R.color.white))
+                        Spacer(modifier = Modifier.height(16.dp))
 
-        pulsingAnimation = Animations.createPulsingAnimation(binding.logo)
+                        Card(
+                            modifier = Modifier.fillMaxWidth(),
+//                            elevation = CardElevation() 4.dp,
+                            shape = MaterialTheme.shapes.medium
+                        ) {
+                            FlowRow {
+                                // Collect answer chunks and update the UI
+                                val answerChunks =
+                                    viewModel.allChunks.collectAsStateWithLifecycle()
+//                                LaunchedEffect(answerChunks) {
+//                                    answerChunks.value.forEach { newChunk ->
+//                                        delay(48)
+                                Text(answerChunks.value.joinToString(""))
+//                                    }
+                            }
 
-        // Collect answer chunks and update the UI
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-
-                viewModel.bufferedAnswerChunks.collect { newChunk ->
-//                    answerChunks.clear()
-                    Log.d("mainactivity", "MAINACTIVITY got chunk: $newChunk")
-                    Log.d("mainactivity", "MAINACTIVITY CHUNKS: $answerChunks")
-                    binding.answerCard.visibility = View.VISIBLE
-
-                    // Remove the previous caret if it exists
-                    removeCaret()
-                    delay(48)
-                    answerChunks.append(newChunk)
-
-                    // Append the blinking caret and apply the BlinkingCaretSpan
-                    val caretPosition = answerChunks.length
-                    answerChunks.append("\u2588") // Unicode character for a block caret
-//                                    answerChunks.setSpan(
-//                                        blinkingCaretSpan,
-//                                        caretPosition,
-//                                        caretPosition + 1,
-//                                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-//                                    )
-
-//                                    blinkingCaretAnimator = createCaretAnimator(blinkingCaretSpan)
-
-                    // Update the Markdown view
-                    updateMarkdownView()
-                }
-            }
-        }
-
-
-        lifecycleScope.launch {
-            viewModel.isFetchingAnswer.collect { isFetching ->
-                if (isFetching) {
-                    pulsingAnimation.start()
-                } else {
-                    pulsingAnimation.cancel()
-                    binding.logo.scaleX = 1f
-                    binding.logo.scaleY = 1f
-                    lifecycleScope.launch {
-                        delay(2000)
-                        removeCaret()
-                        updateMarkdownView()
+                        }
                     }
                 }
             }
         }
     }
 
-    private fun updateMarkdownView() {
-        markwon.setMarkdown(
-            binding.markdownView,
-            answerChunks.toString()
-        )
-    }
+
+//     fun oldonCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//
+//        binding.searchQuery.setOnEditorActionListener { _, actionId, event ->
+//            if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+//                (event?.keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN)
+//            ) {
+//                performSearch(query)
+//                true
+//            } else {
+//                false
+//            }
+//        }
+//
+//        blinkingCaretSpan = BlinkingCaretSpan(ContextCompat.getColor(this, R.color.white))
+//
+//        pulsingAnimation = Animations.createPulsingAnimation(binding.logo)
+//
+//        // Collect answer chunks and update the UI
+//        lifecycleScope.launch {
+//            repeatOnLifecycle(Lifecycle.State.STARTED) {
+//
+//                viewModel.bufferedAnswerChunks.collect { newChunk ->
+////                    answerChunks.clear()
+//                    Log.d("mainactivity", "MAINACTIVITY got chunk: $newChunk")
+//                    Log.d("mainactivity", "MAINACTIVITY CHUNKS: $answerChunks")
+//                    binding.answerCard.visibility = View.VISIBLE
+//
+//                    // Remove the previous caret if it exists
+//                    removeCaret()
+//                    delay(48)
+//                    answerChunks.append(newChunk)
+//
+//                    // Append the blinking caret and apply the BlinkingCaretSpan
+//                    val caretPosition = answerChunks.length
+//                    answerChunks.append("\u2588") // Unicode character for a block caret
+////                                    answerChunks.setSpan(
+////                                        blinkingCaretSpan,
+////                                        caretPosition,
+////                                        caretPosition + 1,
+////                                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+////                                    )
+//
+////                                    blinkingCaretAnimator = createCaretAnimator(blinkingCaretSpan)
+//
+//                    // Update the Markdown view
+//                    updateMarkdownView()
+//                }
+//            }
+//        }
+//
+//
+//        lifecycleScope.launch {
+//            viewModel.isFetchingAnswer.collect { isFetching ->
+//                if (isFetching) {
+//                    pulsingAnimation.start()
+//                } else {
+//                    pulsingAnimation.cancel()
+//                    binding.logo.scaleX = 1f
+//                    binding.logo.scaleY = 1f
+//                    lifecycleScope.launch {
+//                        delay(2000)
+//                        removeCaret()
+//                        updateMarkdownView()
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+//    private fun updateMarkdownView() {
+//        markwon.setMarkdown(
+//            binding.markdownView,
+//            answerChunks.toString()
+//        )
+//    }
 
     private fun removeCaret() {
         val previousCaretPosition = answerChunks.lastIndex
@@ -124,11 +215,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun performSearch() {
-        val query = binding.searchQuery.text.toString()
-        binding.answerCard.visibility = View.GONE
-        binding.sourcesView.visibility = View.GONE
-        answerChunks.clear()
+    private fun performSearch(query: String) {
+//        answerChunks.clear()
         if (query.isNotBlank()) {
             viewModel.search(query)
         }
--- app/src/main/java/cx/aphex/chatgpt/MainViewModel.kt
+++ app/src/main/java/cx/aphex/chatgpt/MainViewModel.kt
@@ -8,32 +8,30 @@ import com.aallam.openai.api.chat.ChatCompletionChunk
 import cx.aphex.chatgpt.api.OpenAIClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 @BetaOpenAI
 class MainViewModel : ViewModel() {
-    private val _answerChunks = MutableSharedFlow<String>(replay = 0)
-    val answerChunks: SharedFlow<String>
+    private val _answerChunks = MutableStateFlow<String>("")
+    val answerChunks: StateFlow<String>
         get() = _answerChunks
 
     // Intermediate flow that buffers the chunks
-    val bufferedAnswerChunks: Flow<String> = _answerChunks.buffer()
+    val bufferedAnswerChunks: StateFlow<String> = _answerChunks
 
     private val _isFetchingAnswer = MutableStateFlow(false)
     val isFetchingAnswer: StateFlow<Boolean>
         get() = _isFetchingAnswer
 
-    private val chunks: MutableList<String> = mutableListOf()
+    val allChunks = MutableStateFlow<List<String>>(listOf())
     fun search(query: String) {
         Log.d("search", "search called!!!!")
         viewModelScope.launch(Dispatchers.Main) {
@@ -44,10 +42,10 @@ class MainViewModel : ViewModel() {
                 .onEach { chunk ->
                     chunk.choices.firstOrNull()?.delta?.content?.let { content ->
                         Log.d("search", "${chunk} content: $content")
-                        chunks.add(content)
+                        allChunks.update { it + content }
                         withContext(Dispatchers.Main) {
                             _answerChunks.emit(content)
-                            Log.d("search", "chunks so far: ${chunks}")
+                            Log.d("search", "chunks so far: ${allChunks}")
                         }
                     }
                 }
