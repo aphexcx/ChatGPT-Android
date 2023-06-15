84630fd80fc71e652db5c1ffc7cea6e72af1ec1c
afik.cohen <aphexsa@gmail.com>
2023-06-14 23:33:47 -0700

    aider: Added a Typography object with text styles to Font.kt.
    # Context:
    USER: add a Typograhpy object that defines a bunch of text styles to Font.kt.
    ASSISTANT: To add a Typography object with text styles to the Font.kt file, follow these steps:
    
    1. Import the necessary components:
    
    ```kotlin
    app/src/main/java/cx/aphex/chatgpt/ui/Font.kt
    <<<<<<< ORIGINAL
    import androidx.compose.ui.text.font.FontFamily
    =======
    import androidx.compose.ui.text.font.FontFamily
    import androidx.compose.material3.Typography
    import androidx.compose.ui.text.TextStyle
    import androidx.compose.ui.unit.sp
    >>>>>>> UPDATED
    ```
    
    2. Define the Typography object with text styles:
    
    ```kotlin
    app/src/main/java/cx/aphex/chatgpt/ui/Font.kt
    <<<<<<< ORIGINAL
    val appFontFamily = FontFamily(
    =======
    val appTypography = Typography(
        h1 = TextStyle(
            fontFamily = appFontFamily,
            fontWeight = FontWeight.W800,
            fontSize = 30.sp
        ),
        h2 = TextStyle(
            fontFamily = appFontFamily,
            fontWeight = FontWeight.W700,
            fontSize = 24.sp
        ),
        h3 = TextStyle(
            fontFamily = appFontFamily,
            fontWeight = FontWeight.W600,
            fontSize = 20.sp
        ),
        body1 = TextStyle(
            fontFamily = appFontFamily,
            fontWeight = FontWeight.W400,
            fontSize = 16.sp
        ),
        body2 = TextStyle(
            fontFamily = appFontFamily,
            fontWeight = FontWeight.W400,
            fontSize = 14.sp
        ),
        caption = TextStyle(
            fontFamily = appFontFamily,
            fontWeight = FontWeight.W400,
            fontSize = 12.sp
        )
    )
    
    val appFontFamily = FontFamily(
    >>>>>>> UPDATED
    ```
    
    Now you have a Typography object named `appTypography` with various text styles defined. You can use these text styles in your Composables by referencing `appTypography`.

--- app/src/main/java/cx/aphex/chatgpt/ui/Font.kt
+++ app/src/main/java/cx/aphex/chatgpt/ui/Font.kt
@@ -2,9 +2,45 @@ package cx.aphex.chatgpt.ui
 
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import cx.aphex.chatgpt.R
+
+val appTypography = Typography(
+    h1 = TextStyle(
+        fontFamily = appFontFamily,
+        fontWeight = FontWeight.W800,
+        fontSize = 30.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = appFontFamily,
+        fontWeight = FontWeight.W700,
+        fontSize = 24.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = appFontFamily,
+        fontWeight = FontWeight.W600,
+        fontSize = 20.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = appFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = appFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = appFontFamily,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp
+    )
+)
 
 val appFontFamily = FontFamily(
     fonts = listOf(
