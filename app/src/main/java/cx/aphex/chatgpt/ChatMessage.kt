99fb2e6a3ff901b4ae72e2784ee3f087833ae756
afik.cohen <aphexsa@gmail.com>
2023-06-14 23:01:31 -0700

    Refactor ChatMessage UI to show profile image and name.

--- app/src/main/java/cx/aphex/chatgpt/ChatMessage.kt
+++ app/src/main/java/cx/aphex/chatgpt/ChatMessage.kt
@@ -4,9 +4,6 @@ package cx.aphex.chatgpt
 
 import androidx.compose.foundation.Image
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.ColorMatrix
-import androidx.compose.ui.graphics.colorspace.ColorSpaces
-import androidx.compose.ui.graphics.colorspace.transferParameters
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,23 +18,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aallam.openai.api.BetaOpenAI
 import com.aallam.openai.api.chat.ChatMessage
 import com.aallam.openai.api.chat.ChatRole
 import dev.jeziellago.compose.markdowntext.MarkdownText
 
+private fun ChatMessage.getNameOrFromRole(useGPT4: Boolean): String? = when (role) {
+    ChatRole.User -> "YOU"
+    ChatRole.Assistant -> if (useGPT4) "GPT-4" else "GPT-3.5"
+    else -> name
+}
+
+
 private val ChatMessage.profileImage: Int?
     get() = when (role) {
-        ChatRole.User -> {
-            android.R.drawable.stat_sys_headset
-        }
-
-        ChatRole.Assistant -> {
-            R.drawable.logo_chatgpt
-        }
-
+        ChatRole.User -> android.R.drawable.stat_sys_headset
+        ChatRole.Assistant -> R.drawable.logo_chatgpt
         else -> null
     }
 
@@ -47,27 +48,40 @@ fun ChatMessage(message: ChatMessage, useGPT4: Boolean) {
     Surface(
         modifier = Modifier.fillMaxWidth()
     ) {
-        Row(
-            modifier = Modifier.padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            message.profileImage?.let {
-                val colorMatrix = ColorMatrix().apply {
-                    val color = if (useGPT4) Color(0xFF4A148C) else Color(0xFF4CAF50)
-                    setColorFilter(color.transferParameters(ColorSpaces.Srgb))
+        Column() {
+            Row(
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 8.dp),
+                verticalAlignment = Alignment.Top,
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    message.profileImage?.let {
+                        val profileTint = if (useGPT4) Color(0xFF4A148C) else Color(0xFF4CAF50)
+                        Image(
+                            painter = painterResource(id = it),
+                            contentDescription = "Profile",
+                            modifier = Modifier
+                                .size(24.dp)
+                                .padding(2.dp)
+                                .clip(CircleShape),
+                            colorFilter = ColorFilter.tint(profileTint)
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(4.dp))
+
+                    message.getNameOrFromRole(useGPT4)?.let {
+                        Text(
+                            text = it,
+                            color = Color.LightGray,
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 }
-                Image(
-                    painter = painterResource(id = it),
-                    contentDescription = "Profile",
-                    modifier = Modifier
-                        .size(32.dp)
-                        .clip(CircleShape),
-                    colorFilter = ColorFilter.colorMatrix(colorMatrix)
-                )
             }
-            Spacer(modifier = Modifier.width(8.dp))
-            Column() {
-                message.name?.let { Text(it) }
+
+            Column(modifier = Modifier.padding(start = 36.dp, top = 0.dp, end = 16.dp)) {
                 MarkdownText(markdown = message.content)
             }
         }
