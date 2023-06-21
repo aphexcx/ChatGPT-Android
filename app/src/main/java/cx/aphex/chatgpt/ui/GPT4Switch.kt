c09d5179edc64e760adb304ad3a588745e8603e0
Afik <2283165+aphexcx@users.noreply.github.com>
2023-06-21 00:34:00 -0700

    Added a shadow to gpt4 switch (#6)

--- app/src/main/java/cx/aphex/chatgpt/ui/GPT4Switch.kt
+++ app/src/main/java/cx/aphex/chatgpt/ui/GPT4Switch.kt
@@ -10,8 +10,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -24,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -40,132 +41,136 @@ fun GPT4Switch(
     gptColor: Color
 ) {
     val viewModel: MainViewModel = viewModel()
-    Box(
-        modifier = Modifier
-            .padding(16.dp)
+    Column(
+        Modifier
+            .fillMaxSize()
             .clip(RoundedCornerShape(6.dp))
-            .background(Color.Gray)
+            .padding(16.dp)
+            .shadow(8.dp, shape = RoundedCornerShape(6.dp))
     ) {
-        Row(
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(64.dp),
-            horizontalArrangement = if (useGPT4) Arrangement.End else Arrangement.Start
-        ) {
-            AnimatedVisibility(
-                visible = !useGPT4,
-                enter = slideInHorizontally(
-                    animationSpec = tween(
-                        durationMillis = 200,
-                    )
-                ) { it },
-                exit = slideOutHorizontally(
-                    animationSpec = tween(
-                        durationMillis = 200,
-                    )
-                ) { 0 }) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .height(64.dp)
-                        .padding(4.dp)
-                        .clip(RoundedCornerShape(6.dp))
-                        .background(gptColor)
-                )
-            }
-            AnimatedVisibility(visible = useGPT4,
-                enter = slideInHorizontally(
-                    tween(
-                        durationMillis = 200,
-                    )
-                ) { -it },
-                exit = slideOutHorizontally(
-                    animationSpec = tween(
-                        durationMillis = 200,
-                    )
-                ) { -it }
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .height(64.dp)
-                        .padding(4.dp)
-                        .clip(RoundedCornerShape(6.dp))
-                        .background(gptColor)
-                )
-            }
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(64.dp)
                 .clip(RoundedCornerShape(6.dp))
-                .background(Color.Transparent),
-            verticalAlignment = Alignment.CenterVertically,
+                .background(Color.LightGray)
         ) {
-            Column(
+            Row(
                 modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .padding(4.dp)
-                    .clip(RoundedCornerShape(6.dp))
-                    .clickable { viewModel.setUseGPT4(false) },
-                verticalArrangement = Arrangement.Center
+                    .fillMaxWidth()
+                    .height(64.dp),
+                horizontalArrangement = if (useGPT4) Arrangement.End else Arrangement.Start
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Icon(
-                        painterResource(R.drawable.bolt),
-                        contentDescription = "GPT-3.5 Icon",
+                AnimatedVisibility(
+                    visible = !useGPT4,
+                    enter = slideInHorizontally(
+                        animationSpec = tween(
+                            durationMillis = 200,
+                        )
+                    ) { it },
+                    exit = slideOutHorizontally(
+                        animationSpec = tween(
+                            durationMillis = 200,
+                        )
+                    ) { 0 }) {
+                    Box(
                         modifier = Modifier
-                            .padding(start = 8.dp, end = 2.dp)
-                            .size(32.dp),
-                        tint = Color.White
+                            .fillMaxWidth(0.5f)
+                            .height(64.dp)
+                            .padding(4.dp)
+                            .clip(RoundedCornerShape(6.dp))
+                            .background(gptColor)
                     )
-                    Text(
-                        "GPT-3.5",
-                        textAlign = TextAlign.Start,
-                        color = Color.White,
-                        style = if (useGPT4) MaterialTheme.typography.displayLarge else MaterialTheme.typography.labelLarge,
+                }
+                AnimatedVisibility(visible = useGPT4,
+                    enter = slideInHorizontally(
+                        tween(
+                            durationMillis = 200,
+                        )
+                    ) { -it },
+                    exit = slideOutHorizontally(
+                        animationSpec = tween(
+                            durationMillis = 200,
+                        )
+                    ) { -it }
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .height(64.dp)
+                            .padding(4.dp)
+                            .clip(RoundedCornerShape(6.dp))
+                            .background(gptColor)
                     )
                 }
             }
-            Column(
+            Row(
                 modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .padding(4.dp)
+                    .fillMaxWidth()
+                    .height(64.dp)
                     .clip(RoundedCornerShape(6.dp))
-                    .clickable { viewModel.setUseGPT4(true) },
-                verticalArrangement = Arrangement.Center
+                    .background(Color.Transparent),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                Row(
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .padding(4.dp)
+                        .clip(RoundedCornerShape(6.dp))
+                        .clickable { viewModel.setUseGPT4(false) },
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Icon(
-                        painterResource(R.drawable.awesome),
-                        contentDescription = "GPT-4 Icon",
+                    Row(
                         modifier = Modifier
-                            .padding(8.dp)
-                            .size(32.dp),
-                        tint = Color.White
-                    )
-                    Text(
-                        "GPT-4",
-                        textAlign = TextAlign.Start,
-                        color = Color.White,
-                        style = if (useGPT4) MaterialTheme.typography.labelLarge else MaterialTheme.typography.displayLarge,
-                    )
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Icon(
+                            painterResource(R.drawable.bolt),
+                            contentDescription = "GPT-3.5 Icon",
+                            modifier = Modifier
+                                .padding(start = 8.dp, end = 2.dp)
+                                .size(32.dp),
+                            tint = Color.White
+                        )
+                        Text(
+                            "GPT-3.5",
+                            textAlign = TextAlign.Start,
+                            color = Color.White,
+                            style = if (useGPT4) MaterialTheme.typography.displayLarge else MaterialTheme.typography.labelLarge,
+                        )
+                    }
+                }
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .padding(4.dp)
+                        .clip(RoundedCornerShape(6.dp))
+                        .clickable { viewModel.setUseGPT4(true) },
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Icon(
+                            painterResource(R.drawable.awesome),
+                            contentDescription = "GPT-4 Icon",
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .size(32.dp),
+                            tint = Color.White
+                        )
+                        Text(
+                            "GPT-4",
+                            textAlign = TextAlign.Start,
+                            color = Color.White,
+                            style = if (useGPT4) MaterialTheme.typography.labelLarge else MaterialTheme.typography.displayLarge,
+                        )
+                    }
                 }
             }
         }
     }
-
-    Spacer(modifier = Modifier.height(16.dp))
-
 }
