85546263a31435cd0091dedee98884280f962793
afik.cohen <aphexsa@gmail.com>
2023-10-16 19:19:11 -0700

    Bump dependencies and update system prompt with the real ChatGPT one

--- app/src/main/java/cx/aphex/chatgpt/api/OpenAIClient.kt
+++ app/src/main/java/cx/aphex/chatgpt/api/OpenAIClient.kt
@@ -13,24 +13,39 @@ import cx.aphex.chatgpt.BuildConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import kotlin.time.Duration.Companion.seconds
 
 @BetaOpenAI
 object OpenAIClient {
-    val config = OpenAIConfig(
+    private val config = OpenAIConfig(
         token = BuildConfig.OPENAI_API_KEY,
         timeout = Timeout(socket = 60.seconds),
     )
 
-    val openAI = OpenAI(config)
+    private val openAI = OpenAI(config)
 
-    fun generateAnswer(prompt: String, history: List<ChatMessage>, useGPT4: Boolean): Flow<ChatCompletionChunk> {
+    fun generateAnswer(
+        prompt: String,
+        history: List<ChatMessage>,
+        useGPT4: Boolean
+    ): Flow<ChatCompletionChunk> {
         val chatCompletionRequest = ChatCompletionRequest(
             model = if (useGPT4) ModelId("gpt-4") else ModelId("gpt-3.5-turbo"),
             messages = listOf(
-                ChatMessage(
+                ChatMessage( //system prompt from https://news.ycombinator.com/item?id=37879077
                     role = ChatRole.System,
-                    content = "You are a helpful assistant that accurately answers the user's queries based on the given text."
+                    content = "You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\n" +
+                            "You are chatting with the user via the ChatGPT Android app. This means most of the time your lines should be a sentence or two, unless the user's request requires reasoning or long-form outputs. Never use emojis, unless explicitly asked to.\n" +
+                            "Knowledge cutoff: 2022-01" +
+                            "Current date: ${
+                                SimpleDateFormat(
+                                    "yyyy-MM-dd",
+                                    Locale.getDefault()
+                                ).format(Date())
+                            }"
                 )
             )
                     + history
